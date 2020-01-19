d77c5003e4e3c40eb8270363852c655d01374bd6
Hyunhong Kim <pol4bear@gmail.com>
2020-01-20 04:17:43 +0900

    Add NetfilterManager

---
+++ Src/NetfilterManager.cpp
@@ -0,0 +1,114 @@
+#include "NetfilterManager.h"
+
+namespace pol4b {
+NetfilterManager::NetfilterManager(OnError cb_on_error_in)
+    : handle(nullptr), queue_handle(nullptr), fd(0),
+      flag_started(false), cb_on_error(cb_on_error_in){}
+
+int NetfilterManager::default_callback(nfq_q_handle *queue_handle, nfgenmsg *message,
+                                       nfq_data *netfilter_data, void *data) {
+    nfqnl_msg_packet_hdr *packet_header = nfq_get_msg_packet_hdr(netfilter_data);
+    if (packet_header == nullptr) throw Error::NFQ_GET_PACKET_HEADER;
+    int id = ntohl(packet_header->packet_id);
+    return nfq_set_verdict(queue_handle, id, NF_ACCEPT, 0, nullptr);
+}
+
+void *NetfilterManager::loop(void *obj_pointer) {
+    NetfilterManager *handle = (NetfilterManager*)obj_pointer;
+
+    try {
+        while(handle->flag_started) {
+            handle->received_size = recv(handle->fd, handle->buf, sizeof(buf), 0);
+
+            if(handle->received_size >= 0) {
+                nfq_handle_packet(handle->handle, handle->buf, handle->received_size);
+                continue;
+            }
+            else if(errno == ENOBUFS) {
+                handle->on_error(Error::NFQ_LOSING_PACKETS);
+                continue;
+            }
+
+            handle->on_error(Error::NFQ_RECV);
+            break;
+        }
+    }
+    catch(int error_code) { handle->on_error(error_code); }
+
+    return nullptr;
+}
+
+bool NetfilterManager::is_started() {
+    return flag_started;
+}
+
+void NetfilterManager::start(uint16_t queue) {
+    try {
+        start(DEFAULT_ADDRESS_FAMILY, queue, nullptr, nullptr);
+    } catch(int error_code) { throw error_code; }
+}
+
+void NetfilterManager::start(nfq_callback *callback, void *data) {
+    try {
+        start(DEFAULT_ADDRESS_FAMILY, DEFAULT_QUEUE_NUM, callback, data);
+    } catch(int error_code) { throw error_code; }
+}
+
+void NetfilterManager::start(uint16_t address_family, uint16_t queue) {
+    try {
+        start(address_family, queue, nullptr, nullptr);
+    } catch(int error_code) { throw error_code; }
+}
+
+void NetfilterManager::start(uint16_t queue, nfq_callback *callback, void *data) {
+    try {
+        start(DEFAULT_ADDRESS_FAMILY, queue, callback, data);
+    } catch(int error_code) { throw error_code; }
+}
+
+void NetfilterManager::start(uint16_t address_family, uint16_t queue, nfq_callback *callback, void *data) {
+    if (flag_started)
+        throw Error::ALREADY_STARTED;
+
+    try {
+        open(address_family, queue, callback, data);
+        if (pthread_create(&job, nullptr, loop, this) != 0)
+            throw Error::PTHREAD_CREATE;
+        if (pthread_detach(job) != 0)
+            throw Error::PTHREAD_DETACH;
+    } catch(int error_code) { throw error_code; }
+
+    flag_started = true;
+}
+
+void NetfilterManager::stop() {
+    flag_started = false;
+}
+
+void NetfilterManager::open(uint16_t address_family, uint16_t queue, nfq_callback *callback, void *data) {
+    handle = nfq_open();
+    if (handle == nullptr)
+        throw Error::NFQ_OPEN;
+    if (nfq_unbind_pf(handle, address_family) < 0)
+        throw Error::NFQ_UNBIND;
+    if (nfq_bind_pf(handle, address_family) < 0)
+        throw Error::NFQ_BIND;
+
+    if (callback == nullptr)
+        queue_handle = nfq_create_queue(handle, queue, &default_callback, data);
+    else
+        queue_handle = nfq_create_queue(handle, queue, callback, data);
+
+    if (queue_handle == nullptr)
+        throw Error::NFQ_CREATE_QUEUE;
+    if (nfq_set_mode(queue_handle, NFQNL_COPY_PACKET, 0xFFFF) < 0)
+        throw Error::NFQ_SET_MODE;
+
+    fd = nfq_fd(handle);
+}
+
+void NetfilterManager::on_error(int error_code) {
+    if (cb_on_error != nullptr)
+        cb_on_error(error_code);
+}
+}
---
+++ Src/NetfilterManager.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <cerrno>
+#include <climits>
+#include <cstdint>
+#include <functional>
+#include <pthread.h>
+#include <unordered_map>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <netinet/if_ether.h>
+#include <netinet/tcp.h>
+#include <linux/netfilter.h>
+extern "C" {
+#include <libnetfilter_queue/libnetfilter_queue.h>
+#include <libnetfilter_queue/libnetfilter_queue_ipv4.h>
+#include <libnetfilter_queue/libnetfilter_queue_tcp.h>
+#include <libnetfilter_queue/libnetfilter_queue_udp.h>
+#include <libnetfilter_queue/pktbuff.h>
+}
+
+namespace pol4b {
+class NetfilterManager
+{
+public:
+    using OnError = std::function<void(int)>;
+
+    NetfilterManager(OnError cb_on_error_in = nullptr);
+
+    static const uint16_t DEFAULT_ADDRESS_FAMILY = AF_INET;
+    static const uint16_t DEFAULT_QUEUE_NUM = 0;
+
+private:
+    static int default_callback(nfq_q_handle *queue_handle, nfgenmsg *message, nfq_data *netfilter_data, void *data);
+    static void *loop(void *obj_pointer);
+
+public:
+    bool is_started();
+
+    void start(uint16_t queue = DEFAULT_QUEUE_NUM);
+    void start(nfq_callback *callback, void *data);
+    void start(uint16_t queue, nfq_callback *callback, void *data);
+    void start(uint16_t address_family, uint16_t queue);
+    void start(uint16_t address_family, uint16_t queue, nfq_callback *callback, void *data);
+    void stop();
+
+private:
+    nfq_handle *handle;
+    nfq_q_handle *queue_handle;
+    int fd;
+    char buf[PATH_MAX];
+    int received_size;
+    pthread_t job;
+
+    void open(uint16_t address_family, uint16_t queue, nfq_callback *callback, void *data);
+
+private:
+    // Flags
+    bool flag_started;
+
+    OnError cb_on_error;
+
+    void on_error(int error_code);
+
+public:
+    class Error {
+    public:
+        enum Code {
+            ALREADY_STARTED = 0x0,
+            NFQ_OPEN,
+            NFQ_UNBIND,
+            NFQ_BIND,
+            NFQ_CREATE_QUEUE,
+            NFQ_SET_MODE,
+            PTHREAD_CREATE,
+            PTHREAD_DETACH,
+            NFQ_LOSING_PACKETS,
+            NFQ_RECV,
+            NFQ_GET_PACKET_HEADER,
+            NFQ_GET_PAYLOAD,
+            NFQ_PKTB_ALLOC,
+            NFQ_SET_TRANSPORT_HEADER,
+            NFQ_GET_TCP_HEADER,
+            NFQ_GET_TCP_PAYLOAD
+        };
+    };
+};
+}
+
+

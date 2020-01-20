ce3edbe4c97f6ee0b585160e1358751240b4b591
Hyunhong Kim <pol4bear@gmail.com>
2020-01-21 00:13:48 +0900

    Update main.cpp

--- Src/main.cpp
+++ Src/main.cpp
@@ -5,13 +5,7 @@
 using namespace std;
 using namespace pol4b;
 
-static void on_error(int error_code) {
-    switch(error_code) {
-    case NetfilterManager::Error::NFQ_LOSING_PACKETS:
-        LogManager::on_error("Losing Packets");
-        break;
-    }
-}
+void on_error(int error_code);
 
 int main(int argc, char *argv[]) {
     LogManager lm(argv[0]);
@@ -28,9 +22,51 @@ int main(int argc, char *argv[]) {
 
     try {
         data_changer.start(queue_number);
+        LogManager::on_info("TcpDataChanger Started!");
     } catch(int error_code) { on_error(error_code); }
 
     while(data_changer.is_started());
 
     return 0;
+}
+
+void on_error(int error_code) {
+    switch(error_code) {
+    case NetfilterManager::Error::ALREADY_STARTED:
+        LogManager::on_error("TcpDataChanger already started");
+        break;
+    case NetfilterManager::Error::NFQ_OPEN:
+        LogManager::on_fatal("Cannot open NFQ");
+        break;
+    case NetfilterManager::Error::NFQ_UNBIND:
+        LogManager::on_fatal("Cannot unbind NFQ");
+        break;
+    case NetfilterManager::Error::NFQ_BIND:
+        LogManager::on_fatal("Cannot bind NFQ");
+        break;
+    case NetfilterManager::Error::NFQ_CREATE_QUEUE:
+        LogManager::on_error("Cannot create NFQ queue");
+        break;
+    case NetfilterManager::Error::NFQ_SET_MODE:
+        LogManager::on_error("Cannot set NFQ mode");
+        break;
+    case NetfilterManager::Error::PTHREAD_CREATE:
+        LogManager::on_error("Cannot create pthread");
+        break;
+    case NetfilterManager::Error::PTHREAD_DETACH:
+        LogManager::on_error("Cannot detatch pthread");
+        break;
+    case NetfilterManager::Error::NFQ_LOSING_PACKETS:
+        LogManager::on_error("Losing Packets");
+        break;
+    case NetfilterManager::Error::NFQ_RECV:
+        LogManager::on_error("Cannot receive packet from NFQ");
+        break;
+    case NetfilterManager::Error::NFQ_GET_PACKET_HEADER:
+        LogManager::on_error("Cannot get packet header");
+        break;
+    case NetfilterManager::Error::NFQ_GET_PAYLOAD:
+        LogManager::on_error("Cannot get payload");
+        break;
+    }
 }

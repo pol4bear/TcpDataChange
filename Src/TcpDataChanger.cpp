6852761167275fe8d4a21c1331dfb2adf82ba9c5
Pol4bear <pol4bear@gmail.com>
2020-01-22 15:02:22 +0900

    Update TcpDataChanger

--- Src/TcpDataChanger.cpp
+++ Src/TcpDataChanger.cpp
@@ -17,39 +17,50 @@ int TcpDataChanger::data_change(nfq_q_handle *queue_handle, nfgenmsg *message, n
     uint8_t *packet = nullptr;
     int packet_length = nfq_get_payload(netfilter_data, &packet);
     if (packet_length < 0) { ACCEPT_AND_THROW(NetfilterManager::Error::NFQ_GET_PAYLOAD); }
-    else if (ntohs(packet_header->hw_protocol) != ETHERTYPE_IP) return nfq_set_verdict(queue_handle, ntohl(packet_header->packet_id), NF_ACCEPT, 0, nullptr);
 
-    TcpPacket tcp_packet(packet, packet_length);
-    flow_manager.assign(tcp_packet.ip_header, tcp_packet.tcp_header);
+    if (ntohs(packet_header->hw_protocol) == ETHERTYPE_IP) {
+        TcpPacket tcp_packet(packet, packet_length);
+        if(!tcp_packet.is_parsed()) return nfq_set_verdict(queue_handle, ntohl(packet_header->packet_id), NF_ACCEPT, 0, nullptr);
+        iphdr *ip_header = tcp_packet.ip_header;
+        tcphdr *tcp_header = tcp_packet.tcp_header;
 
-    list<uint8_t> payload(tcp_packet.payload, tcp_packet.payload + tcp_packet.payload_length);
-    uint32_t left_bytes = tcp_packet.payload_length;
-    for (auto payload_pointer = payload.begin(); payload_pointer != payload.end(); payload_pointer++, left_bytes--) {
-        for (auto it = word_map.begin(); it != word_map.end(); it++) {
-            string key = it->first;
-            string value = it->second;
+        flow_manager.assign(tcp_packet.ip_header, tcp_packet.tcp_header);
 
-            if (left_bytes >= key.length() && *payload_pointer == key[0] && equal(payload_pointer, next(payload_pointer, key.length()), key.begin(), key.end())) {
-                int gap = value.length() - key.length();
-                int index = 0;
-                int index_max = key.length() < value.length() ? key.length() : value.length();
-                for (; index < index_max; index++) { *payload_pointer = value[index]; advance(payload_pointer, 1); }
-                if (gap > 0)
-                    for (; gap > 0; gap--)  { payload.insert(payload_pointer, value[index++]); advance(payload_pointer, 1); }
-                else
-                    for (; gap < 0; gap++)  { payload_pointer = payload.erase(payload_pointer); left_bytes--; }
+        tcp_seq seq = 0;
+        tcp_seq ack = 0;
+        if(!flow_manager.get_sequence_numbers(seq, ack, ip_header, tcp_header)) {
+            seq = tcp_header->th_seq;
+            ack = tcp_header->th_ack;
+        } 
+
+        list<uint8_t> payload(tcp_packet.payload, tcp_packet.payload + tcp_packet.payload_length);
+        uint32_t left_bytes = tcp_packet.payload_length;
+        for (auto payload_pointer = payload.begin(); payload_pointer != payload.end(); payload_pointer++, left_bytes--) {
+            for (auto it = word_map.begin(); it != word_map.end(); it++) {
+                string key = it->first;
+                string value = it->second;
+
+                if (left_bytes >= key.length() && *payload_pointer == key[0] && equal(payload_pointer, next(payload_pointer, key.length()), key.begin(), key.end())) {
+                    int gap = value.length() - key.length();
+                    int index = 0;
+                    int index_max = key.length() < value.length() ? key.length() : value.length();
+                    for (; index < index_max; index++) { *payload_pointer = value[index]; advance(payload_pointer, 1); }
+                    if (gap > 0)
+                        for (; gap > 0; gap--)  { payload.insert(payload_pointer, value[index++]); advance(payload_pointer, 1); }
+                    else
+                        for (; gap < 0; gap++)  { payload_pointer = payload.erase(payload_pointer); left_bytes--; }
+                }
             }
         }
+
+        tcp_header->seq = htonl(seq);
+        tcp_header->th_ack = htonl(ack);
+        flow_manager.modulate(ip_header, tcp_header, payload.size());
+        tcp_packet.set_payload(payload);
+       return nfq_set_verdict(queue_handle, ntohl(packet_header->packet_id), NF_ACCEPT, tcp_packet.packet_length, tcp_packet.packet);
     }
 
-    tcp_seq seq_diff, ack_diff;
-    if(flow_manager.get_sequence_diffs(seq_diff, ack_diff, tcp_packet.src, tcp_packet.dst)) {
-        tcp_packet.tcp_header->th_seq += seq_diff;
-        tcp_packet.tcp_header->th_ack += ack_diff;
-    }
-   flow_manager.apply(tcp_packet.src, tcp_packet.dst, payload.size() - tcp_packet.payload_length);
-   tcp_packet.set_payload(payload);
-   return nfq_set_verdict(queue_handle, ntohl(packet_header->packet_id), NF_ACCEPT, tcp_packet.packet_length, tcp_packet.packet);
+    return nfq_set_verdict(queue_handle, ntohl(packet_header->packet_id), NF_ACCEPT, 0, nullptr);
 }
 
 bool TcpDataChanger::is_started() {
